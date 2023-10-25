f224eef29cdfa641182acc080099abb771364a0a
Lyndsie Lane <84822234+lyndsielane@users.noreply.github.com>
2023-10-25 15:29:39 -0400

    book page styled

--- src/pages/Books/Books.tsx
+++ src/pages/Books/Books.tsx
@@ -12,8 +12,27 @@ function Books() {
         </div>
         <div className="buyLink">COMING SOON</div>
       </div>
+      <div className="vertical-row"></div>
       <div>
-        <div className="bookTitle">Field of Innocence</div>
+        <div className="bookTitle">Hollow of Treason</div>
+        <div className="seriesTitle">The Euphoria Series</div>
+        <div className="bookCover">
+          <img src={comingSoon} alt="book cover" />
+        </div>
+        <div className="buyLink">COMING SOON</div>
+      </div>
+      <div className="vertical-row"></div>
+      <div>
+        <div className="bookTitle">Meadow of Ephiphany</div>
+        <div className="seriesTitle">The Euphoria Series</div>
+        <div className="bookCover">
+          <img src={comingSoon} alt="book cover" />
+        </div>
+        <div className="buyLink">COMING SOON</div>
+      </div>
+      <div className="vertical-row"></div>
+      <div>
+        <div className="bookTitle">Garden of Resolve</div>
         <div className="seriesTitle">The Euphoria Series</div>
         <div className="bookCover">
           <img src={comingSoon} alt="book cover" />
