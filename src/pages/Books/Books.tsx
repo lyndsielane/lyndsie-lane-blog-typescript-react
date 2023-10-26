955600f88329b252afbff143044ed4939fedde9f
Lyndsie Lane <84822234+lyndsielane@users.noreply.github.com>
2023-10-26 10:10:00 -0400

    added all books to book page

--- src/pages/Books/Books.tsx
+++ src/pages/Books/Books.tsx
@@ -3,41 +3,53 @@ import comingSoon from "../../assets/images/ComingSoon.png";
 
 function Books() {
   return (
-    <div className="BooksItem">
-      <div className="topRow">
-        <div className="bookTitle">Field of Innocence</div>
-        <div className="seriesTitle">The Euphoria Series</div>
-        <div className="bookCover">
-          <img src={comingSoon} alt="book cover" />
+    <div>
+      <div className="BooksItem">
+        <div className="stillYours">
+          <div className="bookTitle">I'm Still Yours</div>
+          <div className="seriesTitle">
+            When Hope Rises from the Ashes of Darkness
+          </div>
+          <div className="bookCover">
+            <img src={comingSoon} alt="book cover" />
+          </div>
+          <div className="buyLink">COMING SOON</div>
         </div>
-        <div className="buyLink">COMING SOON</div>
       </div>
-      <div className="vertical-row"></div>
-      <div>
-        <div className="bookTitle">Hollow of Treason</div>
-        <div className="seriesTitle">The Euphoria Series</div>
-        <div className="bookCover">
-          <img src={comingSoon} alt="book cover" />
+      <div className="verticalRow"></div>
+      <div className="BooksItem">
+        <div className="stillYours">
+          <div className="bookTitle">Field of Innocence</div>
+          <div className="seriesTitle">The Euphoria Series</div>
+          <div className="bookCover">
+            <img src={comingSoon} alt="book cover" />
+          </div>
+          <div className="buyLink">COMING SOON</div>
         </div>
-        <div className="buyLink">COMING SOON</div>
-      </div>
-      <div className="vertical-row"></div>
-      <div>
-        <div className="bookTitle">Meadow of Ephiphany</div>
-        <div className="seriesTitle">The Euphoria Series</div>
-        <div className="bookCover">
-          <img src={comingSoon} alt="book cover" />
+        <div className="stillYours">
+          <div className="bookTitle">Hollow of Treason</div>
+          <div className="seriesTitle">The Euphoria Series</div>
+          <div className="bookCover">
+            <img src={comingSoon} alt="book cover" />
+          </div>
+          <div className="buyLink">COMING SOON</div>
         </div>
-        <div className="buyLink">COMING SOON</div>
-      </div>
-      <div className="vertical-row"></div>
-      <div>
-        <div className="bookTitle">Garden of Resolve</div>
-        <div className="seriesTitle">The Euphoria Series</div>
-        <div className="bookCover">
-          <img src={comingSoon} alt="book cover" />
+        <div className="stillYours">
+          <div className="bookTitle">Meadow of Ephiphany</div>
+          <div className="seriesTitle">The Euphoria Series</div>
+          <div className="bookCover">
+            <img src={comingSoon} alt="book cover" />
+          </div>
+          <div className="buyLink">COMING SOON</div>
         </div>
-        <div className="buyLink">COMING SOON</div>
+        <div className="stillYours">
+          <div className="bookTitle">Garden of Resolve</div>
+          <div className="seriesTitle">The Euphoria Series</div>
+          <div className="bookCover">
+            <img src={comingSoon} alt="book cover" />
+          </div>
+          <div className="buyLink">COMING SOON</div>
+        </div>
       </div>
     </div>
   );
