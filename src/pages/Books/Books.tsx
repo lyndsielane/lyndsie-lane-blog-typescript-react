a700b521760713b925ec58a7dcc09830213b1869
Lyndsie Lane <84822234+lyndsielane@users.noreply.github.com>
2023-11-29 16:50:58 -0500

    removed Euphoria books

--- src/pages/Books/Books.tsx
+++ src/pages/Books/Books.tsx
@@ -16,45 +16,6 @@ function Books() {
           <div className="buyLink">COMING SOON</div>
         </div>
       </div>
-      <hr></hr>
-      <div className="verticalRow"></div>
-      <div className="BooksItem">
-        <div className="stillYours">
-          <div className="bookTitle">Field of Innocence</div>
-          <div className="seriesTitle">The Euphoria Series</div>
-          <div className="bookCover">
-            <img src={comingSoon} alt="book cover" />
-          </div>
-          <div className="buyLink">COMING SOON</div>
-        </div>
-        <div className="verticalRow"></div>
-        <div className="stillYours">
-          <div className="bookTitle">Hollow of Treason</div>
-          <div className="seriesTitle">The Euphoria Series</div>
-          <div className="bookCover">
-            <img src={comingSoon} alt="book cover" />
-          </div>
-          <div className="verticalRow"></div>
-          <div className="buyLink">COMING SOON</div>
-        </div>
-        <div className="stillYours">
-          <div className="bookTitle">Meadow of Ephiphany</div>
-          <div className="seriesTitle">The Euphoria Series</div>
-          <div className="bookCover">
-            <img src={comingSoon} alt="book cover" />
-          </div>
-          <div className="verticalRow"></div>
-          <div className="buyLink">COMING SOON</div>
-        </div>
-        <div className="stillYours">
-          <div className="bookTitle">Garden of Resolve</div>
-          <div className="seriesTitle">The Euphoria Series</div>
-          <div className="bookCover">
-            <img src={comingSoon} alt="book cover" />
-          </div>
-          <div className="buyLink">COMING SOON</div>
-        </div>
-      </div>
     </div>
   );
 }
