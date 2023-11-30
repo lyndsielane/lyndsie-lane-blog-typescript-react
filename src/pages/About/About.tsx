8f90135ccdaa2fb9a81b9ca182fa2040c1cd5eb6
Lyndsie Lane <84822234+lyndsielane@users.noreply.github.com>
2023-11-30 11:12:36 -0500

    expanding bio

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -7,12 +7,21 @@ function About() {
       <span className="name lastName">Lane</span>
       <p>
         Hello, I'm Lyndsie Lane, a Christian, wife, and mother with a passion
-        for the written word. I am a full time business analyst and write in my
-        spare time, when I find some. I am a chronic pain warrior with
+        for the written word. I am a full-time business analyst and write in my
+        spare time when I find some. I am a chronic pain warrior with
         Fibromyalgia, Arthritis, Degenerative Disc Disease, and IBS. I'm also a
         mental health warrior dealing with Bipolar 1, PTSD, and generalized and
         social anxiety.
       </p> 
+      <p>
+        Lyndsie Lane is a Christian girl who loves to keep her head in the clouds while 
+        attempting to keep her feet on the ground. She has severe addictions to coffee, tea, 
+        reading, writing, and Autumn and is completely awe-stricken with dragonflies.<br></br> 
+        She spends her days chasing around two crazy kids, three rambunctious dogs, and a house 
+        dragon while attempting to fit as much reading and writing as she can into the hectic mom life.
+        <br></br>Lyndsie hopes her writing illuminates hope for all her readers as she looks for 
+        the silver lining in all of life. 
+      </p>
     </div>
   );
 }
