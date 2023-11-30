bf96a7b8f5a71edab25c59ff8a8fefb1104632f8
Lyndsie Lane <84822234+lyndsielane@users.noreply.github.com>
2023-11-30 10:52:18 -0500

    update about me

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -12,27 +12,7 @@ function About() {
         Fibromyalgia, Arthritis, Degenerative Disc Disease, and IBS. I'm also a
         mental health warrior dealing with Bipolar 1, PTSD, and generalized and
         social anxiety.
-      </p>
-      <p>
-        I am currently working on writing my story of trauma, love, loss, grief,
-        and so many things in between it all. The blurb is below, I hope to
-        publish it by the end of 2024. Enjoy.
-      </p>
-      <p>
-        Can hope survive in the midst of unimaginable tragedy?<br></br>
-        Losing a child is every parents’ worst nightmare. Yet that brutal
-        reality is exactly what Lyndsie Lane was forced to endure. <br></br>
-        After watching her young daughter lose her battle with Cystic Fibrosis,
-        Lyndsie hunted for hope in her darkest hour. That was when she learned a
-        lesson that has carried her through every day since. Turning to the
-        light in hard times is a choice, and not an easy one. But survival
-        depends on it.<br></br>
-        Lyndsie's faith was tested time and again. Still, it was the fire of the
-        Holy Spirit within her that led Lyndsie through the darkness of despair.
-        <br></br>
-        Join Lyndsie as she shares her story. One of unlikely heroes, overcoming
-        challenges, and a special type of magic called hope.
-      </p>
+      </p> 
     </div>
   );
 }
