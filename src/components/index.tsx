c3b0c403ffea1b94c980efdba1f6cc7a3f1a8e42
Lyndsie Lane <84822234+lyndsielane@users.noreply.github.com>
2024-03-12 15:28:30 -0400

    fixed-warnings

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,6 +2,6 @@ import Footer from "./footer/Footer";
 import NavBar from "./navBar/NavBar";
 import NavBarItem from "./navBarItem/NavBarItem";
 import ResumeItem from "./resumeItem/ResumeItem";
-import BooksItem from "./projectssItem/ProjectsItem";
+import ProjectsItem from "./projectsItem/ProjectsItem";
 
-export { Footer, NavBar, NavBarItem, ResumeItem, BooksItem };
+export { Footer, NavBar, NavBarItem, ResumeItem, ProjectsItem };
