c73c30e5382e597c6d34e252a5ce95deda9d7f4f
Lyndsie Lane <84822234+lyndsielane@users.noreply.github.com>
2024-03-12 13:18:49 -0400

    updated-bio

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -6,21 +6,20 @@ function About() {
       <span className="name firstName">Lyndsie</span>
       <span className="name lastName">Lane</span>
       <p>
-        Hello, I'm Lyndsie Lane, a Christian, wife, and mother with a passion
-        for the written word. I am a full-time business analyst and write in my
-        spare time when I find some. I am a chronic pain warrior with
-        Fibromyalgia, Arthritis, Degenerative Disc Disease, and IBS. I'm also a
-        mental health warrior dealing with Bipolar 1, PTSD, and generalized and
-        social anxiety.
-      </p> 
+        Hello, I'm Lyndsie Lane, a Christian, wife, and mother. I am a full-time
+        business analyst and Web Developer by design. After spending 11 years in
+        the Customer Service industry, I stepped into the Technical world,
+        bringing a customer-first approach to my web design abilities. I have a
+        heart for adding accessibility into all sites.
+      </p>
       <p>
-        Lyndsie Lane is a Christian girl who loves to keep her head in the clouds while 
-        attempting to keep her feet on the ground. She has severe addictions to coffee, tea, 
-        reading, writing, and Autumn and is completely awe-stricken with dragonflies.<br></br> 
-        She spends her days chasing around two crazy kids, three rambunctious dogs, and a house 
-        dragon while attempting to fit as much reading and writing as she can into the hectic mom life.
-        <br></br>Lyndsie hopes her writing illuminates hope for all her readers as she looks for 
-        the silver lining in all of life. 
+        When I'm not working or designing, I'm a simple Christian girl who keeps
+        her head in the clouds while attempting to keep her feet on the ground.
+        I have severe addictions to coffee, tea, reading, and Autumn and am
+        completely awe-stricken with dragonflies.<br></br>I spend my days
+        chasing around two crazy kids, three rambunctious dogs, and a house
+        dragon while attempting to fit as much reading and relaxing as I can
+        into the hectic mom life.
       </p>
     </div>
   );
