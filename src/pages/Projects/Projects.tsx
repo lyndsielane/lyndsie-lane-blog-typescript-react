073191673f239d2e9b805c807f520ce7d947bd00
Lyndsie Lane <84822234+lyndsielane@users.noreply.github.com>
2024-03-16 18:06:26 -0400

    2 projects added

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,5 +1,7 @@
 import "./Projects.scss";
 import imgPlaceholder from "../../assets/images/ImgPlaceholder.png";
+import portfolioImg from "../../assets/images/Portfolio.png";
+import parallaxImg from "../../assets/images/Parallax Site.png";
 import { Link } from "react-router-dom";
 
 function Projects() {
@@ -7,62 +9,53 @@ function Projects() {
     <div>
       <div className="ProjectsItem">
         <div>
-          <div className="ProjectsTitle">Project 4</div>
-          <div className="projectsDescription">project description here</div>
+          <div className="ProjectsTitle">Parallax Website</div>
+          <div className="projectsDescription">
+            A basic Parallax Website desing using HTML & CSS.
+          </div>
           <div className="projectScreenshot">
-            <img src={imgPlaceholder} alt="screenshot of project" />
+            <img src={parallaxImg} alt="screenshot of project" />
           </div>
           <div className="linksContainer">
             <div className="repoLink">
-              <a href="/#">Repo</a>
+              <a
+                href="https://github.com/lyndsielane/html-css-parallaxsite"
+                target="_blank"
+              >
+                Repo
+              </a>
             </div>
             <div className="siteLink">
-              <a href="/#">Site</a>
+              <a
+                href="https://lyndsielane.github.io/html-css-parallaxsite/"
+                target="_blank"
+              >
+                Site
+              </a>
             </div>
           </div>
         </div>
         <div>
-          <div className="ProjectsTitle">Project 3</div>
-          <div className="projectsDescription">project description here</div>
+          <div className="ProjectsTitle">Portfolio</div>
+          <div className="projectsDescription">
+            This portfolio created using React and Typescript.
+          </div>
           <div className="projectScreenshot">
-            <img src={imgPlaceholder} alt="screenshot of project" />
+            <img src={portfolioImg} alt="screenshot of project" />
           </div>
           <div className="linksContainer">
             <div className="repoLink">
-              <a href="/#">Repo</a>
+              <a
+                href="https://github.com/lyndsielane/lyndsie-lane-blog-typescript-react/tree/main/public"
+                target="_blank"
+              >
+                Repo
+              </a>
             </div>
             <div className="siteLink">
-              <a href="/#">Site</a>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="ProjectsTitle">Project 2</div>
-          <div className="projectsDescription">project description here</div>
-          <div className="projectScreenshot">
-            <img src={imgPlaceholder} alt="screenshot of project" />
-          </div>
-          <div className="linksContainer">
-            <div className="repoLink">
-              <a href="/#">Repo</a>
-            </div>
-            <div className="siteLink">
-              <a href="/#">Site</a>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="ProjectsTitle">Project 1</div>
-          <div className="projectsDescription">project description here</div>
-          <div className="projectScreenshot">
-            <img src={imgPlaceholder} alt="screenshot of project" />
-          </div>
-          <div className="linksContainer">
-            <div className="repoLink">
-              <a href="/#">Repo</a>
-            </div>
-            <div className="siteLink">
-              <a href="/#">Site</a>
+              <a href="https://lyndsielane.com/" target="_blank">
+                Site
+              </a>
             </div>
           </div>
         </div>
